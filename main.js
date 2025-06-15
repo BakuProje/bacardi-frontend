9f3de5bf8f7f47e64135575755a278e22cd75664
BakuProje <muhfaizsyeh@gmial.com>
2025-06-15 12:28:38 +0800

    BacardiPS UPDATE 2

--- main.js
+++ main.js
@@ -656,24 +656,6 @@ document.addEventListener('DOMContentLoaded', () => {
     document.head.appendChild(style);
 });
 
-function createSnowflake() {
-  const snowflake = document.createElement('div');
-  snowflake.classList.add('snowflake');
-  snowflake.innerHTML = '❅';
-  snowflake.style.left = Math.random() * 100 + 'vw';
-  snowflake.style.animationDuration = Math.random() * 3 + 2 + 's';
-  snowflake.style.opacity = Math.random();
-  snowflake.style.fontSize = Math.random() * 10 + 10 + 'px';
-  
-  document.body.appendChild(snowflake);
-  
-  setTimeout(() => {
-    snowflake.remove();
-  }, 5000);
-}
-
-setInterval(createSnowflake, 100);
-
 function markAdminMessagesAsRead() {
     const adminMessages = document.querySelectorAll('.message.admin');
     if (adminMessages.length > 0 && currentReportId) {
